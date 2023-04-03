adcc3b08d4ddc3e5f845b5506d6db59a58d3e783
zikot <->
2023-04-03 20:57:41 +0300

    added EF connection to MySQL

---
+++ Backend/Repositories/UserRepository.cs
@@ -0,0 +1,28 @@
+﻿using Backend.DbConfigurations;
+using Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Repositories
+{
+    public class UserRepository
+    {
+        private ApplicationContext context;
+
+        public UserRepository(ApplicationContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IEnumerable<User>> GetAllUsers() 
+        {
+            return await context.Users.ToListAsync();
+        }
+
+        public async Task AddUser(User user)
+        {
+            await context.Users.AddAsync(user);
+            await context.SaveChangesAsync();
+        }
+
+    }
+}
